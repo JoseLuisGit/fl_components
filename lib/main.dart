26b7ff6e06d8c0c7eb053c26d2c3ac6d903c58db
JoseLuisGit <joseluisstacruz@gmail.com>
2022-01-18 21:09:56 -0400

    add routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-//import 'package:fl_components/screens/listview1_screen.dart';
-import 'package:fl_components/screens/listview2_screen.dart';
+import 'screens/screens.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,9 +9,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Listview2Screen()
+      initialRoute: 'listview2',
+      routes: {
+        'card': (BuildContext context) => const CardScreen(),
+        'alert': (BuildContext context) => const AlertScreen(),
+        'listview1': (BuildContext context) => const Listview1Screen(),
+        'listview2': (BuildContext context) => const Listview2Screen(),
+      }
     );
   }
 }
