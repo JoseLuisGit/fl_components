cc7ccd93c5f8e80459fe02e7bb05606b27d99d30
JoseLuisGit <joseluisstacruz@gmail.com>
2022-01-18 20:48:52 -0400

    use list view separated with divider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:fl_components/screens/listview1_screen.dart';
+//import 'package:fl_components/screens/listview1_screen.dart';
+import 'package:fl_components/screens/listview2_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Material App',
-      home: Listview1Screen()
+      home: Listview2Screen()
     );
   }
 }
---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Listview2Screen extends StatelessWidget {
+  
+  final options = const ['Megaman', 'Metal Gear', 'Super Smash', 'Final Fantasy', 'Yolanda'];
+   
+  const Listview2Screen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('List View 2'),
+      ),
+      body: ListView.separated(
+        itemCount: options.length,
+        itemBuilder: (context, index){
+          return ListTile(
+            title: Text(options[index]),
+            trailing: const Icon(Icons.arrow_right) 
+          ,);
+        },
+        separatorBuilder: (_, __)=> const Divider()
+        )
+    );
+  }
+}
