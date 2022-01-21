7db5515dc536d2285c953efe1e6a857b510ac85c
JoseLuisGit <joseluisstacruz@gmail.com>
2022-01-20 20:39:21 -0400

    Fix size Image loading

--- lib/widgets/custom_card_2.dart
+++ lib/widgets/custom_card_2.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 
@@ -6,12 +7,28 @@ class CustomCard2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Card(
-      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-      elevation: 10,
-      child: FadeInImage(
-          image: NetworkImage('https://wallpaper.dog/large/17026288.jpg'),
-          placeholder: AssetImage('assets/loading.gif')),
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      elevation: 20,
+      shadowColor: AppTheme.primary,
+      child: Column(
+        children:  [
+          const FadeInImage(
+              image: NetworkImage('https://wallpaper.dog/large/17026288.jpg'),
+              placeholder: AssetImage('assets/loading.gif'),
+              width: double.infinity,
+              height: 220,
+              fit: BoxFit.cover,
+              fadeInDuration: Duration( milliseconds: 300 ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(8.0),
+                alignment: Alignment.bottomRight,
+                child: const Text('Imagen de Internet'),
+              )
+        ],
+      ),
     );
   }
 }
