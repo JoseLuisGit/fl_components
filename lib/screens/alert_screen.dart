a2e1cd1ee1aee0708a23c7d8f70e558ba064756a
JoseLuisGit <joseluisstacruz@gmail.com>
2022-01-21 21:41:48 -0400

    create show alert

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -1,17 +1,40 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
    
   const AlertScreen({Key? key}) : super(key: key);
   
+
+  void showAlert(BuildContext context){
+    showDialog(
+      context: context, 
+      builder: (context){
+        return AlertDialog(
+          title:const Text('This is an Alert'),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text('Exercitation reprehenderit sunt ipsum proident ex sint deserunt. Exercitation id pariatur quis in aliqua nostrud culpa nostrud ut est voluptate exercitation id aliqua. Aliqua commodo cupidatat tempor dolore nulla consequat quis commodo est ad. Cupidatat nostrud id aute nostrud esse nisi labore adipisicing ut eu dolore magna ut ipsum. Pariatur laboris nulla ex laborum incididunt occaecat aliqua laboris deserunt excepteur exercitation.'),
+              SizedBox(height: 8 ),
+              FlutterLogo(size: 100,),
+            ],
+          ),
+          actions: [
+            TextButton(onPressed: ()=>Navigator.pop(context), child: const Text('Cancel')),
+            TextButton(onPressed: ()=>Navigator.pop(context), child: const Text('Ok')),
+          ],
+        );
+      });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          onPressed: (){
-
-          }, 
+          onPressed: ()=>showAlert(context), 
           child: const Padding(
             padding:  EdgeInsets.all(8.0),
             child:  Text('Show Alert', style: TextStyle( fontSize: 15),),
