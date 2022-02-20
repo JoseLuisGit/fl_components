62cd94655cbbfd4e622fb80fc688d5f1bf7b5ab8
JoseLuisGit <joseluisstacruz@gmail.com>
2022-02-20 10:41:42 -0400

    add screen list view

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 import 'package:fl_components/screens/screens.dart';
@@ -17,6 +18,7 @@ class AppRoutes{
     MenuOption(icon: Icons.animation, route: 'animated', title: 'This is a Animated Screen', screen: const AnimatedContainerScreen()),
     MenuOption(icon: Icons.input, route: 'input', title: 'This is a Input', screen: const InputScreen()),
     MenuOption(icon: Icons.slideshow, route: 'slider', title: 'This is a Slider', screen: const SliderScreen()),
+    MenuOption(icon: Icons.list, route: 'listview', title: 'This is a listview', screen: const ListViewScreen()),
     
   ];
 
