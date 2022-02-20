fc493e8038df62af55a59f48f3341bd0afd0b256
JoseLuisGit <joseluisstacruz@gmail.com>
2022-02-20 17:09:37 -0400

    add image loading

--- lib/screens/list_view_screen.dart
+++ lib/screens/list_view_screen.dart
@@ -6,9 +6,21 @@ class ListViewScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('ListViewScreen'),
+    return MediaQuery.removePadding(
+      context: context,
+      removeTop: true,
+      removeBottom: true,
+      child: Scaffold(
+        body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (_, index){
+            return FadeInImage(
+              placeholder: const AssetImage('assets/load.gif'),
+              image:  NetworkImage('https://picsum.photos/500/300?image=${index +1}'),
+                    
+            );
+          }
+          )
       ),
     );
   }
